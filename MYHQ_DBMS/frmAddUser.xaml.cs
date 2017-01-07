5c09824c5171c8484af97c9f4916913aa19f6d2d
PrincessGod <jihongjiang@outlook.com>
2017-01-07 19:14:33 +0800

    Little Sample

---
+++ MYHQ_DBMS/frmAddUser.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XAIS.Branchs.XICT2DW.Login
+{
+    /// <summary>
+    /// frmSingelTargetPlaybacks.xaml 的交互逻辑
+    /// </summary>
+    public partial class frmAddUser : Window
+    {
+        public frmAddUser()
+        {
+            InitializeComponent();
+        }
+
+        #region 窗口标题栏
+        /// <summary>
+        /// 拖动窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        int i = 0;
+        /// <summary>
+        /// 标题栏双击事件
+        /// </summary>
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            i += 1;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += (s, e1) => { timer.IsEnabled = false; i = 0; };
+            timer.IsEnabled = true;
+
+            if (i % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                i = 0;
+                this.WindowState = this.WindowState == WindowState.Maximized ?
+                              WindowState.Normal : WindowState.Maximized;
+            }
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //设置窗口最小化
+        }
+
+        /// <summary>
+        /// 窗口最大化与还原
+        /// </summary>
+        private void btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal; //设置窗口还原
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized; //设置窗口最大化
+            }
+        }
+
+        #endregion
+
+
+        private void ButtonQuery_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Button_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //this.m_View.BackState();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.PassWord.Password.ToCharArray().Length < 6)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(500);
+                }).ContinueWith(t =>
+                {
+                    MainSnackbar.MessageQueue.Enqueue("您输入的密码少于6位!");
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+                return;
+            }
+            if (this.PassWord.Password != this.SurePs.Password)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(500);
+                }).ContinueWith(t =>
+                {
+                    MainSnackbar.MessageQueue.Enqueue("两次密码输入不一致!");
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+                return;
+            }
+        }
+
+
+        private void PassWord_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (this.PassWord.Password.ToCharArray().Length < 6)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(500);
+                }).ContinueWith(t =>
+                {
+                    MainSnackbar.MessageQueue.Enqueue("您输入的密码少于6位!");
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+                return;
+            }
+        }
+
+        private void SurePS_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (this.PassWord.Password != this.SurePs.Password)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(500);
+                }).ContinueWith(t =>
+                {
+                    MainSnackbar.MessageQueue.Enqueue("两次密码输入不一致!");
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+                return;
+            }
+        }
+    }
+}
---
+++ MYHQ_DBMS/frmBackStageManagement.xaml.cs
@@ -0,0 +1,266 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace XAIS.Branchs.XICT2DW.Login
+{
+    /// <summary>
+    /// frmTracksControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class frmBackStageManagement : Window
+    {
+        public frmBackStageManagement()
+        {
+            InitializeComponent();
+        }
+
+        #region 窗口标题栏
+        /// <summary>
+        /// 拖动窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        int i = 0;
+        /// <summary>
+        /// 标题栏双击事件
+        /// </summary>
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            i += 1;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += (s, e1) => { timer.IsEnabled = false; i = 0; };
+            timer.IsEnabled = true;
+
+            if (i % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                i = 0;
+                this.WindowState = this.WindowState == WindowState.Maximized ?
+                              WindowState.Normal : WindowState.Maximized;
+            }
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //设置窗口最小化
+        }
+
+        /// <summary>
+        /// 窗口最大化与还原
+        /// </summary>
+        private void btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal; //设置窗口还原
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized; //设置窗口最大化
+            }
+        }
+
+        #endregion
+        private void Button_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Test1> t1 = new List<Test1>() 
+            {
+                new Test1(){ V1 = "张三", V2 = DateTime.Now.ToString(), V3 = "删除了一个表格"},
+                new Test1(){ V1 = "张三", V2 = DateTime.Now.ToString(), V3 = "删除了一个表格"},
+                new Test1(){ V1 = "张三", V2 = DateTime.Now.ToString(), V3 = "删除了一个表格"},
+                new Test1(){ V1 = "张三", V2 = DateTime.Now.ToString(), V3 = "删除了一个表格"}
+            };
+
+            List<Test2> t2 = new List<Test2>() 
+            {
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+                new Test2(){ V1 = "张三", V2 = "水电部", V3 = "科员", V4 = "Ripple", V5 = "12asd12444", V6 = "一级"},
+            };
+
+            List<Test3> t3 = new List<Test3>() 
+            {
+                new Test3(){ V1 = "数据库一"},
+                new Test3(){ V1 = "数据库一"},
+                new Test3(){ V1 = "数据库一"},
+            };
+
+            this.DutyManagement.ItemsSource = t1;
+            this.UserManagement.ItemsSource = t2;
+            this.DataBaseManagement.ItemsSource = t3;
+
+            //add BlackoutDates
+            StartDate.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(1), new DateTime(2100, 1, 1)));
+            EndDate.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(1), new DateTime(2100, 1, 1)));
+
+            StartDate.SelectedDate = DateTime.Now.AddDays(-1).Date;
+            StartDate.Text = DateTime.Now.AddDays(-1).Date.ToShortDateString();
+            EndDate.SelectedDate = DateTime.Now.Date;
+
+            StartTime.SelectedTime = DateTime.Now;
+            EndTime.SelectedTime = DateTime.Now;
+        }
+        private void BTSelect_Click(object sender, EventArgs e)
+        {
+          //  this.m_View.ActiveTaskInOwner();
+        }
+        private void BtOK_Click(object sender, EventArgs e)
+        {
+            //if(this.PiHao.Text!=null&&this.CBTracksRange.SelectedValue!=null)
+            //{
+            //    this.TracksControlPiHao = this.PiHao.Text;
+            //    this.TracksControlRange = this.CBTracksRange.Text;
+            //    this.Close();
+            //    MessageBox.Show(this.TracksControlPiHao, this.TracksControlRange);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("您没有选择目标或者范围！");
+            //    return;
+            //}   
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //this.m_View.BackState();
+        }
+
+        private void BTAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            frmAddUser frm = new frmAddUser();
+            frm.Owner = this;
+            frm.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
+            frm.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartTime.SelectedTime != null && EndTime.SelectedTime != null && StartDate.SelectedDate != null && EndDate.SelectedDate != null)
+            {
+                if (StartDate.SelectedDate <= EndDate.SelectedDate)
+                {
+                    if (StartDate.SelectedDate == EndDate.SelectedDate && StartTime.SelectedTime > EndTime.SelectedTime)
+                    {
+                        Task.Factory.StartNew(() =>
+                        {
+                            Thread.Sleep(500);
+                        }).ContinueWith(t =>
+                        {
+                            MainSnackbar.MessageQueue.Enqueue("开始时间不能晚于结束时间!");
+                        }, TaskScheduler.FromCurrentSynchronizationContext());
+
+                        return;
+                    }
+
+                    ///do some thing
+                }
+                else
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        Thread.Sleep(500);
+                    }).ContinueWith(t =>
+                    {
+                        MainSnackbar.MessageQueue.Enqueue("开始时间不能晚于结束时间!");
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+
+                    return;
+                }
+
+            }
+            else
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(500);
+                }).ContinueWith(t =>
+                {
+                    MainSnackbar.MessageQueue.Enqueue("请先输入时间范围");
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+    }
+
+    public class Test1
+    {
+        public string V1 { get; set; }
+
+        public string V2 { get; set; }
+
+        public string V3 { get; set; }
+    }
+
+    public class Test2
+    {
+        public string V1 { get; set; }
+
+        public string V2 { get; set; }
+
+        public string V3 { get; set; }
+
+
+        public string V4 { get; set; }
+
+        public string V5 { get; set; }
+
+        public string V6 { get; set; }
+    }
+
+    public class Test3
+    {
+        public string V1 { get; set; }
+    }
+}
